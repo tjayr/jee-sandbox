deaa321e6a32e72a4b15acab6611c65c05382ad0
Tony Ayres <tayres@gmail.com>
2013-09-28 21:16:42 +0100

    Creating qualifiers for test and production data stores

---
+++ jee-cdi-beans/src/main/java/com/clearprecision/sandbox/cdi/qualifiers/Production.java
@@ -0,0 +1,15 @@
+package com.clearprecision.sandbox.cdi.qualifiers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.enterprise.inject.Stereotype;
+
+@Stereotype
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { ElementType.TYPE, ElementType.FIELD, ElementType.METHOD })
+public @interface Production {
+
+}
