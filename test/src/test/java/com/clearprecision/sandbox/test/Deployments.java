463d6350c8399764818b1058a2ca8c2d30db1b7a
Tony Ayres <tayres@gmail.com>
2013-09-28 19:19:17 +0100

    Updating test Deployments, adding debug profile to maven

--- test/src/test/java/com/clearprecision/sandbox/test/Deployments.java
+++ test/src/test/java/com/clearprecision/sandbox/test/Deployments.java
@@ -11,14 +11,20 @@ import org.jboss.shrinkwrap.resolver.api.maven.MavenResolvedArtifact;
 public class Deployments {
 
 	public static Archive<?> createSandboxEarDeployment() {
-		File file = Maven.resolver()
-				.resolve("com.clearprecision:jee-sandbox-ear:ear")
+		System.setProperty("maven.repo.local", "/home/tony/.m2");
+		File pom = new File("/home/tony/git-projects/jee-sandbox-ear/test/pom.xml");
+		File file = Maven.resolver().offline().loadPomFromFile(pom).importTestDependencies()
+				.resolve("com.clearprecision:jee-sandbox-ear:ear:1.0.1-SNAPSHOT")				
 				.withoutTransitivity().asSingle(MavenResolvedArtifact.class)
 				.asFile();
 
 		EnterpriseArchive ear = ShrinkWrap.createFromZipFile(
 				EnterpriseArchive.class, file);
 		return ear;
+	}
+	
+	public static void main(String[] args) {
+		Deployments.createSandboxEarDeployment();
 	}
 
 }
