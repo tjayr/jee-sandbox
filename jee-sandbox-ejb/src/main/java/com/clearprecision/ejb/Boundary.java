1730ca891968c9d82973b92981c4fa93664a1dc4
Tony Ayres <tayres@gmail.com>
2013-09-29 18:25:33 +0100

    Creating some ejb and cdi beans

---
+++ jee-sandbox-ejb/src/main/java/com/clearprecision/ejb/Boundary.java
@@ -0,0 +1,26 @@
+package com.clearprecision.ejb;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+import com.clearprecision.sandbox.cdi.interfaces.DataStore;
+
+@ApplicationScoped
+public class Boundary {
+
+	private static final Logger logger = org.slf4j.LoggerFactory.getLogger(Boundary.class);
+		
+	@Inject
+	DataStore dataStore;
+	
+	
+	
+	public String getDataStoreName() {
+		String name = dataStore.getName();
+		logger.info("Boundary postcontruct, dataStore created with name {}", name);
+		return name;
+	}
+
+}
---
+++ jee-sandbox-ejb/src/main/java/com/clearprecision/ejb/StartupBean.java
@@ -0,0 +1,25 @@
+package com.clearprecision.ejb;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+@Startup
+@Singleton
+public class StartupBean {
+	
+	private static final Logger logger = org.slf4j.LoggerFactory.getLogger(Boundary.class);
+
+	@Inject
+	Boundary boundary;
+	
+	@PostConstruct
+	void startup() {
+		logger.info("Startup bean post contruct");
+		logger.info("Boundary returned {}", boundary.getDataStoreName());
+	}
+	
+}
